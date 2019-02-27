89f666305b5ddfc148b42be3aa79812c65a9f818
hreitsma <henk@custompixels.nl>
2019-02-27 14:08:32 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,99 +1,154 @@
-function init() {
-	
-	openFB.init({appId: '466211257250990'}); //localstorage instead of sessionstore: // tokenStore: window.localStorage
+var App = (function () {
 
-	if(window.sessionStorage.fbAccessToken) {
-		openFB.getLoginStatus(facebookData);
-	} else {
-		showPanel('.authenticate-panel',false);
-	}
+	var navHistory = [],
 	
+		platform,
+		
+		storage = window.localStorage,
+	
+		panelBehavior = {
+			authenticate: { header: false, footer: false, history: false },
+			maps: { header: true, footer: 'footer-navigation', history: true },
+			capture: { header: false, footer: 'footer-photo', history: true },
+		};
+
 	$('.upload').on('click',function(){
-		showPanel('.capture-panel','.footer-photo');
+		showPanel('capture');
 		$('.nav-item').removeClass('active');
 		$('.nav-item.photo').addClass('active');
 		captureImage();
 	});
-}
-
-function facebookData(response) {
-	console.log('facebook auth check: '+ response.status);
 	
-	if(response.status === 'connected') {
-		openFB.api({path: '/me', 
-			success: function(response){
-				showPanel('.maps-panel','.footer-navigation');
-				$('.username').text(response.name);
-				$('.profile-picture').attr("src", "https://graph.facebook.com/" + response.id + "/picture?type=normal")
-				console.log(response);
-			}, 
-			error: function(err){
-				showPanel('.authenticate-panel',false);
-				openFB.logout();
-			}
-		});
-	} else {
-		showPanel('.authenticate-panel',false);
-	}
-}
+	var backBtn = document.getElementById('back-button');
+	backBtn.addEventListener("click", goBack);
+	document.addEventListener("backbutton", goBack, true);
+	
+	var facebookBtn = document.getElementById('facebook-btn');
+	facebookBtn.addEventListener("click", facebookLogin);
+	
+	var googleBtn = document.getElementById('google-btn');
+	googleBtn.addEventListener("click", googleLogin);
+		
+	function init(params) {
+		
+		if (params.platform) {
+            platform = params.platform;
+        }
+		
+		openFB.init({appId: '466211257250990', tokenStore: window.localStorage});
 
-function facebookLogin() {
-	openFB.login( function(response) {
-			if(response.status === 'connected') {
-				openFB.getLoginStatus(facebookData);
+		// validate authentication tokens
+		if(storage.fbAccessToken) {
+			openFB.getLoginStatus(facebookData);
+		} else {
+			showPanel('authenticate');
+		}
+	}
+	
+	function goBack() {
+		
+		if(navHistory.length > 1) {
+			var panel = navHistory.splice(-2)[0];
+		} else if(platform == 'android' || platform == 'iphone') {
+			navigator.app.exitApp();
+		} else if(window.localStorage.fbAccessToken) {
+			openFB.logout();
+		} else if(false /* check for google login */) {
+			// google logout
+		}
+		
+		showPanel(panel || 'authenticate');
+	}
+
+	function facebookData(response) {
+		console.log('facebook auth check: '+ response.status);
+		
+		if(response.status === 'connected') {
+			openFB.api({path: '/me', 
+				success: function(response){
+					showPanel('maps');
+					$('.username').text(response.name);
+					$('.profile-picture').attr("src", "https://graph.facebook.com/" + response.id + "/picture?type=normal")
+					console.log(response);
+				}, 
+				error: function(err){
+					showPanel('authenticate');
+					openFB.logout();
+				}
+			});
+		} else {
+			showPanel('authenticate');
+		}
+	}
+
+	function facebookLogin() {
+		openFB.login( function(response) {
+				if(response.status === 'connected') {
+					openFB.getLoginStatus(facebookData);
+					
+					console.log('Facebook login succeeded, got access token: ' + response.authResponse.accessToken);
+				} else {
+					alert('Facebook login failed: ' + response.error);
+				}
+			}, {scope: 'public_profile,email'});
+	}
+	
+	function googleLogin() {
+		
+	}
+
+	function captureImage() {
+		navigator.mediaDevices.getUserMedia({
+			'audio': false,
+			'video': { facingMode: { exact: "environment" } }
+		 }).then(function(mediaStream) {
+			
+			var video = document.querySelector('video');
+				video.srcObject = mediaStream;
+				video.onloadedmetadata = function(e) {
+				video.play();
+			};
+			
+			$('.capture-button').on('click',function(mediastream){
 				
-				console.log('Facebook login succeeded, got access token: ' + response.authResponse.accessToken);
-			} else {
-				alert('Facebook login failed: ' + response.error);
-			}
-		}, {scope: 'public_profile,email'});
-}
+				var track = mediaStream.getVideoTracks()[0];
+				var imageCapture = new ImageCapture(track);
+				
+				imageCapture.takePhoto().then(blob => {
+					console.log('Photo taken');
+					// img is an <img> tag
+					var video = $('#capture-stream').hide();
+					const image = document.querySelector('#capture-image');
+					image.src = URL.createObjectURL(blob);
+					$('.capture-button').hide();
+					$('#capture-image').show();
+				})
+				.catch(err => console.error('takePhoto() failed: '));
+			});
+		  
+		 });
+	}
 
-function captureImage() {
-	navigator.mediaDevices.getUserMedia({
-		'audio': false,
-		'video': { facingMode: { exact: "environment" } }
-	 }).then(function(mediaStream) {
+	function showPanel(panel) {
 		
-		var video = document.querySelector('video');
-		    video.srcObject = mediaStream;
-		    video.onloadedmetadata = function(e) {
-			video.play();
-		};
+		console.log('show panel: '+ panel);
+		var b = panelBehavior[panel];
+		if(b.history) { navHistory.push(panel); }
 		
-		$('.capture-button').on('click',function(mediastream){
-			
-			var track = mediaStream.getVideoTracks()[0];
-			var imageCapture = new ImageCapture(track);
-			
-			imageCapture.takePhoto().then(blob => {
-				console.log('Photo taken');
-				// img is an <img> tag
-				var video = $('#capture-stream').hide();
-				const image = document.querySelector('#capture-image');
-				image.src = URL.createObjectURL(blob);
-				$('.capture-button').hide();
-				$('#capture-image').show();
-			})
-		    .catch(err => console.error('takePhoto() failed: '));
-		});
-	  
-	 });
-}
-
-function showPanel(panel,footer) {
-	
-	console.log('show panel: '+ panel +', and footer: '+ footer);
-	
-	if($('.panel-splash').is(':visible')) {
-		$('.panel-splash-background').delay(700).fadeOut();
+		$('.footer').hide();
+		if(b.footer) {
+			$('.'+ b.footer).show();
+		}
+		
+		$('.panel').hide();
+		$('.'+ panel +'-panel').show();
+		
+		console.log(navHistory);
 	}
 	
-	$('.panel').hide();
-	$(panel).show();
+	// The public API
+    return {
+        init: init,
+    }
 	
-	$('.footer').hide();
-	if(footer) {
-		$(footer).show();
-	}
-}
+}());
